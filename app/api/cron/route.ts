4ab504cf848d052c85279efb6ae2190fa551a225
anshumanjain2002 <IIT2020239@iiita.ac.in>
2024-10-16 21:10:45 +0530

    Added commit

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -5,6 +5,9 @@ import { scrapeAmazonProduct } from "@/lib/scraper";
 import { getAveragePrice, getEmailNotifType, getHighestPrice, getLowestPrice } from "@/lib/utils";
 import { NextResponse } from "next/server";
 
+export const maxDuration= 300;
+export const dynamic= 'force-dynamic';
+export const revalidate= 0;
 export async function GET()
 {
     try {
@@ -34,7 +37,7 @@ export async function GET()
                      averagePrice: getAveragePrice(updatedPriceHistory),
                 }
                 const updatedProduct = await Product.findOneAndUpdate( 
-                    { url: scrapedProduct.url},
+                    { url: product.url},
                     product,
                 );
 
